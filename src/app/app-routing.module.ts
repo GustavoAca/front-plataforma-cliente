b2460ba296a7cf1a6c20e4ca9546a86a7285657d
Gustavo dos Anjos <galasdalas50@gmail.com>
2022-06-29 17:52:58 -0300

    Cadastrar concluido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'entrar', pathMatch: 'full' },
 
   { path: 'entrar', component: EntrarComponent },
-  
+  { path: 'cadastrar', component: CadastrarComponent }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
